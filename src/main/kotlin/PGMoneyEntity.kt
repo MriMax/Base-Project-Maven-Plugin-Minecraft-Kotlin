ee74e36e2f1bf33470978e7acd810f77d6ba2fcc
MriMax <119247300+MriMax@users.noreply.github.com>
2026-02-18 18:11:13 +0300

    dd
    ddd

---
+++ src/main/kotlin/PGMoneyEntity.kt
@@ -0,0 +1,15 @@
+package ru.mrimax
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class PGMoneyEntity: JavaPlugin() {
+
+    override fun onEnable() {
+        // Код
+    }
+
+    override fun onDisable() {
+        // Код
+    }
+
+}
